ace4673a3a54bc4a7c99ecef42859977cb4f79ce
Ricardo Cantó <rifcaab@gmail.com>
2020-04-04 21:32:04 +0200

    bash modular

--- src/main/java/com/rcanto/ggrep/App.java
+++ src/main/java/com/rcanto/ggrep/App.java
@@ -4,20 +4,29 @@
 package com.rcanto.ggrep;
 
 import java.io.*;
+import java.util.Vector;
 public class App
 {
 	public static void main(String[] args)
 	{
-		String linea;
-		File f = null;
-		FileReader fr = null;
 		if(args.length < 2)
 		{
 			System.out.println("Debes especificar el patrón y el nombre del fichero");
 			System.exit(0);
 		}
+		Vector<String> v = compruebaLineas(args[1],args[0]);
+		for (String s: v)
+			System.out.println(s);
+	}
 
-		f = new File(args[1]);
+	public static Vector<String> compruebaLineas(String file,String patr)
+	{
+		Vector<String> v = new Vector<String>();
+		String linea;
+		File f = null;
+		FileReader fr = null;
+
+		f = new File(file);
 		try
 		{
 			fr = new FileReader(f);
@@ -30,13 +39,16 @@ public class App
 		try
 		{
 			while((linea = br.readLine()) != null)
-				if(linea.contains(args[0]))	// args[0] és el patró de búsqueda
-				    System.out.println(linea);
-	        if (br != null) { br.close(); fr.close(); }
+				if(linea.contains(patr))	// args[0] és el patró de búsqueda
+				    //System.out.println(linea);
+				    v.add(linea);
+	        	if (br != null) { br.close(); fr.close(); }
+			return v;
 		}
 		catch(IOException e)
 		{
 			System.out.println("IOException");
+			return null;
 		}
 	}
 }
